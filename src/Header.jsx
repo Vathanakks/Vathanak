bf10da8a2829baa4d6ec45b55a1978a8f26e7904
Vathanak <keatsopheavatanak0@gmail.com>
2026-04-06 08:17:13 +0700

    fix: resolve vercel build error

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,9 +5,9 @@ const Header = () => {
 <header className="header" >
     <nav>
         <ul className="nav-list">
-            <li><i class="fa fa-home" aria-hidden="true"></i><a href="/">Home</a></li>
-            <li><a href="/">My SKill</a></li>
-            <li><a href="/">About</a></li>
+            <li><i class="fa fa-home" aria-hidden="true"></i><a href="/skill-container">Home</a></li>
+            <li><a href="/skill-container">My SKill</a></li>
+            <li><a href="/skill-container">About</a></li>
         </ul>
     </nav>
 
--- src/Link.jsx
+++ src/Link.jsx
@@ -7,28 +7,28 @@ const Link = () => {
             <h1 className="profile-text" >My Profile Link <i class="fa fa-link" aria-hidden="true"></i></h1>
             <a href="https://www.facebook.com/vathanakks" className="link-box" target="_blank" rel="noopener noreferrer">
                 <div className="link-logo">
-                <img src="facebook-logo.avif" alt=""></img>
+                <img src="facebook-logo.avif" alt="FB"></img>
                 </div>
                 <h1 className="link-box-text">Facebook</h1>
             </a>
 
             <a href="https://pay.ababank.com/oRF8/9pai2k5y" className="link-box" target="_blank" rel="noopener noreferrer">
                 <div className="link-logo">
-                <img src="unnamed.png" alt=""></img>
+                <img src="unnamed.png" alt="ABA"></img>
                 </div>
                 <h1 className="link-box-text">ABA</h1>
             </a>
 
             <a href="https://pay.ababank.com/oRF8/9pai2k5y" className="link-box" target="_blank" rel="noopener noreferrer">
                 <div className="link-logo">
-                <img src="logo.png" alt=""></img>
+                <img src="logo.png" alt="AC"></img>
                 </div>
                 <h1 className="link-box-text">Acleda</h1>
             </a>
 
             <a href="https://t.me/vathanakks" className="link-box" target="_blank" rel="noopener noreferrer">
                 <div className="link-logo">
-                <img src="Telegram_logo.svg.png" alt=""></img>
+                <img src="Telegram_logo.svg.png" alt="Telegram"></img>
                 </div>
                 <h1 className="link-box-text">Telegram</h1>
             </a>
